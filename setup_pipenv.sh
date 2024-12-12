f9460bee0279641166609e3ca68016fa5cef1d21
Roelof Rietbroek <r.rietbroek@utwente.nl>
2024-12-12 15:39:06 +0100

    add install script to make/reuse platform dpendent pip environments

---
+++ setup_pipenv.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+# create an architecture dependent pip environment to run shxarray tutorials and install the jupyter kernels as a user 
+#  Run this script in the folder where you want to create the pip environments
+#R.Rietbroek Dec 2024
+
+
+envname=pysh_`uname -m`
+
+scriptpath=`dirname -- "$0"`
+
+
+if [ -d "$envname" ]; then
+    echo "Environment $envname  already exists"
+    
+else
+    echo "$envname creating new pip environment, this can take a while"
+    python -m venv $envname
+    #install required software
+    ${envname}/bin/pip install -r $scriptpath/requirements.txt
+fi
+
+# now we can isntall a jupyter kernel so it can be used in notebooks
+kerneldir=${HOME}/.local/share/jupyter/kernels/$envname
+
+if [ -d "$kerneldir" ]; then
+    echo "Jupyter kernel $kerneldir already exists"
+else
+    echo "installing new Jupyter kernel for the current user"
+    ${envname}/bin/python -m ipykernel install --user --name ${envname} 
+
+fi
+
+
